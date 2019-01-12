368ce953fc8997a39f41601fbe50c86f0c1d6897
Anna Makarovskaya <anutya@mail.ru>
2019-01-12 22:14:48 +0300

    #100DaysOfCode: Day 8. Python

--- magicnumbers.py
+++ magicnumbers.py
@@ -1,4 +1,5 @@
 import random
+
 magic_numbers = [random.randint(0,9), random.randint(0,9)]
 chance = 3
 for attempt in range(chance):
@@ -8,3 +9,13 @@ for attempt in range(chance):
         print("Вы угадали число!")
     if user_number not in magic_numbers:
         print('Вы даже не близко.')
+
+
+minimum=100
+for index in range(10):
+    random_number=random.randint(0,100)
+    print('Сгенерированное число {}'.format(random_number))
+    if random_number < minimum:
+            minimum = random_number
+print('minumum {}'.format(minimum))
+        
