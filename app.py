8603848475031ba37394dd6cabbb3f5b32915b04
Anna Makarovskaya <anutya@mail.ru>
2019-01-22 22:42:28 +0300

    #100DaysOfCode: Day 12: classes

--- app.py
+++ app.py
@@ -1,21 +1,72 @@
-#100DaysOfCode, Day10
-#sample_set = {3, 5, 9, 1, 1}
+#100DaysOfCode, Day10,11
+#100DaysOfCode, Day10,11,12
+student_list = []
 
-student= {'имя':'Jose',
-          'оценки':[0,67,80,44,100],
-          "экзамены": {
-              'final':60,
-              'midterm':50
-          }}
-#print(student['mark'][1])
-print(student['экзамены']['final'])
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.marks = []
+
+    def average_mark(self):
+        number = len(self.marks)
+        if number == 0:
+            return 0
+
+        total = sum(self.marks)
+        return total / number
+
+
 
 def create_student():
     name = input('Введите имя студента: ')
-    student_data = {
-        'name': name,
-        'marks': []
-    }
+    student_data = Student(name)
+
     return student_data
 
-print(create_student())
+
+def add_mark(student, mark):
+    student.marks.append(mark)
+    return None
+
+
+def print_student_details(student):
+    print("{}, средняя оценка: {}.".format(student.name,
+                                         student.average_mark()))
+
+
+def print_student_list(students):
+    for i, student in enumerate(students):
+        print("ID: {}".format(i))
+        print_student_details(student)
+
+def menu():
+    selection = input("Введите 'p' для выведения на экран списка студентов, \n"
+                      "'s' для добавления нового студента, 'a' для добавления \n "
+                      "оценки или 'q' для выхода: \n")
+
+    while selection != 'q':
+        if selection == 'p':
+            print_student_list(student_list)
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'a':
+            student_id = int(input("Введите ID студента, которому хотите добавить оценки: "))
+            student = student_list[student_id]
+            new_mark = int(input('Введите новую оценку: '))
+            add_mark(student, new_mark)
+
+        selection = input("Введите 'p' для выведения на экран списка студентов, \n"
+                          "'s' для добавления нового студента, 'a' для добавления \n "
+                          "оценки или 'q' для выхода: \n")
+
+menu()
+
+'''
+import sys
+my_vars = []
+for i in range(3):
+    my_vars.append(lambda: i)
+#print([f() for f in my_vars])
+print(sys.argv)
+
+'''
