8a6be2e3efd017f441ed3e7237943bbfa32532ef
Anna Makarovskaya <anutya@mail.ru>
2019-03-08 14:04:15 +0300

    #100DaysOfCode. Day 24,25 Python + PostgreSQL

--- databases/app.py
+++ databases/app.py
@@ -1,5 +1,12 @@
 from user import User
-
-
-my_user = User('spaoffice@mail.ru', 'Anton', 'Barashka', 1)
+"""
+my_user = User('jose@schoolofcode.com', 'Jose', 'Salvatierra', None)
 print(my_user)
+my_user.save_to_db()
+#-----
+my_user = User.load_from_db_by_email('jose@schoolofcode.com')
+print(my_user)
+
+"""
+my_user = User('rolf@rsmith.com', 'Rolf', 'Smith', None)
+my_user.save_to_db()
--- databases/user.py
+++ databases/user.py
@@ -1,3 +1,4 @@
+from database import connect
 class User:
     def __init__(self, email, first_name, last_name, id):
         self.email = email
@@ -7,3 +8,19 @@ class User:
 
     def __repr__(self):
         return "<User {}>".format(self.email)
+
+    def save_to_db(self):
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                #running some code
+                cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
+                               (self.email, self.first_name, self.last_name))
+    @classmethod
+    def load_from_db_by_email(cls, email):
+        #another_user = cls('rolf@schoolofcode.com', 'Rolf', 'Smith', None)
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                #running some code
+                cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+                user_data = cursor.fetchone()
+                return cls(email=user_data[1], first_name=[2], last_name=[3], id=[0])
